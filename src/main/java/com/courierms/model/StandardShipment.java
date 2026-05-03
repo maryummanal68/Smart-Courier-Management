4661498baf8afd2a246a6a8434f77a8000860d8e
Maryum Manal <manalmaryum@gmail.com>
2026-05-03 20:41:08 +0500

    Add StandardShipment class

---
+++ src/main/java/com/courierms/model/StandardShipment.java
@@ -0,0 +1,18 @@
+package com.courierms.model;
+
+public class StandardShipment extends Shipment{
+    public StandardShipment(String tackingID,String senderName,String receiverName,Double weight,String shipmentDate){
+        super (tackingID,senderName,receiverName,weight,shipmentDate);
+
+    }
+
+    @Override
+    public double cost_calculation() {
+        return getWeight()*50;
+    }
+
+    @Override
+    public int delivery_days() {
+        return 5;
+    }
+}
