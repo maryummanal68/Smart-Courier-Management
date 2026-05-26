7b8f6649e6bf3f168626f503dc4f8e1d1a42a2f5
maryummanal68 <manalmaryum@gmail.com>
2026-05-26 11:36:39 +0500

    Add Admin and Courier to backend model

---
+++ backend/model/Admin.java
@@ -0,0 +1,15 @@
+package com.courierms.model;
+
+public class Admin extends Person {
+    private String adminID;
+    public Admin(String adminID, String name, String email, String phone_no, String password){
+        super (name, email, phone_no, password);
+    }
+    @Override
+    public String getRole(){
+        return "Admin";
+    }
+    public String getAdminID(){
+        return adminID;
+    }
+}
