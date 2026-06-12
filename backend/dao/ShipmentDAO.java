4006925abbee19cbd9c1bf21782b81e551db6d87
maryummanal68 <manalmaryum@gmail.com>
2026-06-12 17:24:46 +0500

    Final submission - complete project

--- backend/dao/ShipmentDAO.java
+++ backend/dao/ShipmentDAO.java
@@ -9,7 +9,7 @@ public class ShipmentDAO {
     public boolean saveShipment(Shipment shipment){
         try{
             Connection con= DBManager.getConnection();
-            String sql_query="INSERT INTO shipments(tackingID, senderName, receiverName, weight, shipmentDate,status)VALUES(?,?,?,?,?,?)";
+            String sql_query="INSERT INTO shipments(tracking_id, sender_name, receiver_name, weight, shipment_date, status)VALUES(?,?,?,?,?,?)";
             PreparedStatement ps = con.prepareStatement(sql_query);
             ps.setString(1, shipment.getTrackingID());
             ps.setString(2, shipment.getSenderName());
@@ -25,34 +25,36 @@ public class ShipmentDAO {
             return false;
         }
     }
-    public Shipment findTrackingID(String trackingID){
+    public Shipment findTrackingID(String trackingID) {
         try {
-            Connection con=DBManager.getConnection();
-            String sql_query="SLECT * FROM shipments WHERE trackingID=?";
-            PreparedStatement ps = con.prepareStatement(sql_query);
+            Connection con = DBManager.getConnection();
+            PreparedStatement ps = con.prepareStatement(
+                    "SELECT * FROM shipments WHERE tracking_id=?"
+            );
             ps.setString(1, trackingID);
             ResultSet rs = ps.executeQuery();
             if (rs.next()) {
                 Shipment s = new Shipment(
-                        rs.getString("trackingID"),
-                        rs.getString("senderName"),
-                        rs.getString("receiverName"),
+                        rs.getString("tracking_id"),
+                        rs.getString("sender_name"),
+                        rs.getString("receiver_name"),
                         rs.getDouble("weight"),
-                        rs.getString("shipmentDate")
+                        rs.getString("shipment_date")
                 ) {
                     @Override
                     public double cost_calculation() { return 0; }
-
                     @Override
                     public int delivery_days() { return 0; }
                 };
+                s.setStatus(rs.getString("status"));
                 return s;
             }
         } catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
         }
         return null;
-        }
-        }
+    }
+}
+
 
 
